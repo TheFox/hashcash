b521aa75f5e5cf81d2cdbd3ce6fe72f15de883fe
Christian Mayer <thefox21at@gmail.com>
2014-04-25 17:38:24 +0200

    Test2 added.

--- tests/SpeedTest.php
+++ tests/SpeedTest.php
@@ -10,32 +10,66 @@ declare(ticks = 1);
 
 const TIME_MAX = 30;
 
+$test1 = 0;
+$test2 = 1;
 
-print "time max: ".TIME_MAX."\n";
+// Test 1
+if($test1){
+	print "time max: ".TIME_MAX."\n";
 
-$exit = false;
-function sig($no){
-	global $exit;
-	$exit = true;
-	#print "ALARM: $no\n";
+	$exit = false;
+	function sig($no){
+		global $exit;
+		$exit = true;
+		#print "ALARM: $no\n";
+	}
+	$sig = pcntl_signal(SIGALRM, 'sig');
+	print "signal setup: ".($sig ? 'ok' : 'failed')."\n";
+
+
+	for($bits = 10; $bits < 52 && !$exit; $bits++){
+		$hashcash = new Hashcash($bits, 'example@example.com');
+		$stamp = '';
+		
+		$start = time();
+		fwrite(STDOUT, 'mint '.$bits.' bits: ');
+		
+		pcntl_alarm(TIME_MAX);
+		$stamp = $hashcash->mint();
+		
+		fwrite(STDOUT, (time() - $start).'sec ');
+		fwrite(STDOUT, '"'.$stamp.'" - '.$hashcash->getHash()."\n");
+		
+		if(!$stamp) break;
+	}
 }
-$sig = pcntl_signal(SIGALRM, 'sig');
-print "signal setup: ".($sig ? 'ok' : 'failed')."\n";
 
+// Test 2
+if($test2){
+	$bits = 19;
+	$seconds = array();
+	for($n = 0; $n < 1000; $n++){
+		$hashcash = new Hashcash($bits, 'example@example.com');
+		$stamp = '';
+		
+		$start = time();
+		fwrite(STDOUT, $n.' mint '.$bits.' bits: ');
+		
+		try{
+			$stamp = $hashcash->mint();
+		}
+		catch(Exception $e){
+			fwrite(STDOUT, 'error ');
+		}
+		
+		$t = time() - $start;
+		$seconds[] = $t;
+		
+		fwrite(STDOUT, $t.'sec ');
+		fwrite(STDOUT, '"'.$stamp.'" - '.$hashcash->getHash()."\n");
+		
+	}
 
-for($bit = 10; $bit < 52 && !$exit; $bit++){
-	$hashcash = new Hashcash($bit, 'example@example.com');
-	$stamp = '';
-	
-	$start = time();
-	fwrite(STDOUT, 'mint '.$bit.' bits: ');
-	
-	
-	pcntl_alarm(TIME_MAX);
-	$stamp = $hashcash->mint();
-	
-	fwrite(STDOUT, (time() - $start).'sec ');
-	fwrite(STDOUT, '"'.$stamp.'" - '.$hashcash->getHash()."\n");
-	
-	if(!$stamp) break;
+	var_export($seconds);
 }
+
