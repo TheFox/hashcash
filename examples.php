6be2abd1f40598343e7d80026ba0ece35426b2f0
Christian Mayer <thefox21at@gmail.com>
2014-04-27 15:17:32 +0200

    Example descriptions.

--- examples.php
+++ examples.php
@@ -5,7 +5,7 @@ require 'vendor/autoload.php';
 use TheFox\Hashcash\Hashcash;
 
 
-// Example 1
+// Example 1: simple mint
 $hashcash = new Hashcash();
 $hashcash->setBits(20);
 $hashcash->setResource('example@example.com');
@@ -17,7 +17,7 @@ catch(Exception $e){
 }
 
 
-// Example 2
+// Example 2a: simple mint, another way
 $hashcash = new Hashcash(20, 'example@example.com');
 $stamp = '';
 try{
@@ -28,6 +28,7 @@ catch(Exception $e){
 	print "ERROR 2: ".$e->getMessage()."\n";
 }
 
+// Example 2b: verify a stamp
 $hashcash = new Hashcash();
 try{
 	print "hashcash stamp2 verify: '".( $hashcash->verify($stamp) ? 'Ok' : 'failed' )."'\n";
@@ -37,7 +38,7 @@ catch(Exception $e){
 }
 
 
-// Example 3
+// Example 3a: use a certain date
 $hashcash = new Hashcash(20, 'example@example.com');
 $hashcash->setDate('870221');
 $stamp = '';
@@ -49,17 +50,18 @@ catch(Exception $e){
 	print "ERROR 4: ".$e->getMessage()."\n";
 }
 
+// Example 3b: verify a stamp, must fail
 $hashcash = new Hashcash();
 $hashcash->setExpiration(3600 * 24 * 2); // Expire in 2 days.
 try{
-	print "hashcash stamp3 verify: '".( $hashcash->verify($stamp) ? 'Ok' : 'failed' )."'\n";
+	print "hashcash stamp3 verify: '".( $hashcash->verify($stamp) ? 'Ok' : 'failed' )."' (must fail)\n";
 }
 catch(Exception $e){
 	print "ERROR 5: ".$e->getMessage()."\n";
 }
 
 
-// Example 4
+// Example 4a: use a certain date
 $hashcash = new Hashcash(20, 'example@example.com');
 $hashcash->setDate('870221');
 $stamp = '';
@@ -71,6 +73,7 @@ catch(Exception $e){
 	print "ERROR 6: ".$e->getMessage()."\n";
 }
 
+// Example 4a: verify a stamp, ignore expiration
 $hashcash = new Hashcash();
 $hashcash->setExpiration(0); // Never expire.
 try{
