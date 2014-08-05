7aa12c35829a059dabd9cdc985b5f28cf35a55f6
Christian Mayer <christian@fox21.at>
2014-08-05 17:57:55 +0200

    Test.

--- tests/AverageTest.php
+++ tests/AverageTest.php
@@ -10,28 +10,37 @@ declare(ticks = 1);
 // Mac: 3.4gz
 //const BITS = 12; // 35 sec (avg: 0.036 seconds)
 //const BITS = 15; // 4:33 min (avg: 0.272 seconds)
-//const BITS = 17; // 18:27 min (avg: 1.108 seconds)
-//const BITS = 19; // 74:16 min (avg: 4.456 seconds)
-const BITS = 20; // 
+//const BITS = 17; // 18 min (avg: 1.108 seconds)
+//const BITS = 19; // 74 min (avg: 4.456 seconds)
+const BITS = 20; // 147 min (avg: 8.856 seconds)
 const TESTS = 1000;
-const TIME_MAX = 60;
+const TIME_MAX = 120;
 
 
+$next = false;
 $exit = false;
 
-function sig(){
+function sig_next(){
+	fwrite(STDOUT, 'next'."\n");
+}
+
+function sig_exit(){
 	global $exit;
 	$exit = true;
 }
 
-$sig = pcntl_signal(SIGALRM, 'sig');
-$sig = pcntl_signal(SIGINT, 'sig');
-$sig = pcntl_signal(SIGTERM, 'sig');
+$sig = pcntl_signal(SIGALRM, 'sig_next');
+print 'signal setup: '.($sig ? 'ok' : 'failed')."\n";
+$sig = pcntl_signal(SIGINT, 'sig_exit');
+print 'signal setup: '.($sig ? 'ok' : 'failed')."\n";
+$sig = pcntl_signal(SIGTERM, 'sig_exit');
 print 'signal setup: '.($sig ? 'ok' : 'failed')."\n";
 
 fwrite(STDOUT, 'bits: '.BITS."\n");
 fwrite(STDOUT, 'tests: '.TESTS."\n");
 
+$diffMin = TIME_MAX;
+$diffMax = 0;
 for($testno = 1; $testno <= TESTS && !$exit; $testno++){
 	$hashcash = new Hashcash(BITS, 'example@example.com');
 	$stamp = '';
@@ -41,8 +50,16 @@ for($testno = 1; $testno <= TESTS && !$exit; $testno++){
 	
 	pcntl_alarm(TIME_MAX);
 	$stamp = $hashcash->mint();
+	pcntl_alarm(null);
 	
 	$diff = time() - $start;
+	if($diff > $diffMax){
+		$diffMax = $diff;
+	}
+	if($diff < $diffMin){
+		$diffMin = $diff;
+	}
+	
 	fwrite(STDOUT, $diff.' sec "'.$stamp.'"   '.$hashcash->getAttempts()."\n");
 	
 	$times[] = $diff;
@@ -52,4 +69,6 @@ for($testno = 1; $testno <= TESTS && !$exit; $testno++){
 
 fwrite(STDOUT, 'bits: '.BITS."\n");
 fwrite(STDOUT, 'tests: '.TESTS."\n");
+fwrite(STDOUT, 'min: '.$diffMin.' seconds'."\n");
+fwrite(STDOUT, 'max: '.$diffMax.' seconds'."\n");
 fwrite(STDOUT, 'avg: '.(array_sum($times) / TESTS).' seconds'."\n");
